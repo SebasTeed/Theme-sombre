c510c06aa7b6703d1c27f0407d686e44bebb1a56
Sebastien <sebastienteed@gmail.com>
2024-04-06 16:19:41 -0400

    Appli terminée

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 
-const bouton = document.querySelector('#mode');
-const theme = document.querySelector(span);
+let button = document.querySelector('#mode');
+let span = document.querySelector('span');
 
+if (localStorage.getItem('theme')){
+    if(localStorage.getItem('theme') == 'sombre'){
+        modeSombre();
+    }
+    
+}
+
+
+
+button.addEventListener('click', () =>{
+    if (document.body.classList.contains('dark')){
+        // Mode clair
+        document.body.className = '';
+        span.textContent = 'Thème sombre';
+        localStorage.setItem('theme', 'clair');
+    }
+    else{
+        //Mode sombre
+        modeSombre();
+    }
+});
+
+function modeSombre(){
+    document.body.className = 'dark';
+    span.textContent = 'Thème clair';
+    localStorage.setItem('theme', 'sombre');
+};
